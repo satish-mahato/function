58bc33a977b679b0b53c36248714ee07023e2b12
satishsingh202dd <satishsingh202dd@gmail.com>
2024-09-13 18:24:06 +0000

    "first commit"

---
+++ function/function1.js
@@ -0,0 +1,7 @@
+// by using this function we can print our function that can be repetedly used
+function greetingToeveryone(){
+    console.log("Hello everyone!");
+    console.log("Good Morning!");
+}
+greetingToeveryone()
+greetingToeveryone()
